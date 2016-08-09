f64b41923e82b5321045b88d15031f7493095bb7
Alexandre Becoulet <alexandre.becoulet@free.fr>
2016-08-09 17:39:16 +0200

    test kex: test more algorithm sets

--- test/kex.c
+++ test/kex.c
@@ -61,59 +61,203 @@ struct algo_with_key_s
   size_t key_size;
 };
 
-static const struct algo_with_key_s kex_list[] = 
-  {
-    { &assh_kex_none,              NULL, NULL, 0 },
-    { &assh_kex_curve25519_sha256, NULL, NULL, 0 },
-    { &assh_kex_m383_sha384,	   NULL, NULL, 0 },
-    { &assh_kex_m511_sha512,	   NULL, NULL, 0 },
-    { &assh_kex_dh_group1_sha1,	   NULL, NULL, 0 },
-    { &assh_kex_dh_group14_sha1,   NULL, NULL, 0 },
-    { &assh_kex_dh_gex_sha1,	   NULL, NULL, 0 },
-    { &assh_kex_dh_gex_sha256_12,  NULL, NULL, 0 },
-    { &assh_kex_dh_gex_sha256_8,   NULL, NULL, 0 },
-    { &assh_kex_rsa1024_sha1,	   &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) },
-    { &assh_kex_rsa2048_sha256,	   &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) },
-    { NULL },
-  };
+static const struct algo_with_key_s kex_list_long[] =
+{
+  { &assh_kex_none,              NULL, NULL, 0 },
+  { &assh_kex_curve25519_sha256, NULL, NULL, 0 },
+  { &assh_kex_m383_sha384,	   NULL, NULL, 0 },
+  { &assh_kex_m511_sha512,	   NULL, NULL, 0 },
+  { &assh_kex_dh_group1_sha1,	   NULL, NULL, 0 },
+  { &assh_kex_dh_group14_sha1,   NULL, NULL, 0 },
+  { &assh_kex_dh_gex_sha1,	   NULL, NULL, 0 },
+  { &assh_kex_dh_gex_sha256_12,  NULL, NULL, 0 },
+  { &assh_kex_dh_gex_sha256_8,   NULL, NULL, 0 },
+  { &assh_kex_rsa1024_sha1,	   &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) },
+  { &assh_kex_rsa2048_sha256,	   &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) },
+  { NULL },
+};
 
-static const struct algo_with_key_s sign_list[] =
-  {
-    { &assh_sign_none,              &assh_key_none, (const uint8_t*)"\0", 0 },
-    { &assh_sign_dsa,               &assh_key_dsa, dsa1024_key, sizeof(dsa1024_key) - 1 },
-    //    { &assh_sign_dsa2048_sha224,    &assh_key_dsa, dsa2048_key, sizeof(dsa2048_key) },
-    { &assh_sign_dsa2048_sha256,    &assh_key_dsa, dsa2048_key, sizeof(dsa2048_key) - 1 },
-    { &assh_sign_dsa3072_sha256,    &assh_key_dsa, dsa3072_key, sizeof(dsa3072_key) - 1 },
-    { &assh_sign_rsa_sha1_md5,      &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) - 1 },
-    { &assh_sign_rsa_sha1,          &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) - 1 },
-    { &assh_sign_rsa_sha1_2048,     &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) - 1 },
-    { &assh_sign_rsa_sha256,        &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) - 1 },
-    { &assh_sign_rsa_sha512,        &assh_key_rsa, rsa3072_key, sizeof(rsa3072_key) - 1 },
-    { &assh_sign_ed25519,           &assh_key_ed25519, ed25519_key, sizeof(ed25519_key) - 1 },
-    { NULL }
-  };
+static const struct algo_with_key_s kex_list_short[] =
+{
+  { &assh_kex_none,              NULL, NULL, 0 },
+  { &assh_kex_curve25519_sha256, NULL, NULL, 0 },
+  { NULL }
+};
 
-static const struct assh_algo_cipher_s *cipher_list[] =
-  {
-    &assh_cipher_none,
-    &assh_cipher_arc4,
-    &assh_cipher_aes128_cbc,
-    &assh_cipher_aes256_ctr,
-    NULL
-  };
+static const struct algo_with_key_s sign_list_long[] =
+{
+  { &assh_sign_none,              &assh_key_none, (const uint8_t*)"\0", 0 },
+  { &assh_sign_dsa,               &assh_key_dsa, dsa1024_key, sizeof(dsa1024_key) - 1 },
+  //    { &assh_sign_dsa2048_sha224,    &assh_key_dsa, dsa2048_key, sizeof(dsa2048_key) },
+  { &assh_sign_dsa2048_sha256,    &assh_key_dsa, dsa2048_key, sizeof(dsa2048_key) - 1 },
+  { &assh_sign_dsa3072_sha256,    &assh_key_dsa, dsa3072_key, sizeof(dsa3072_key) - 1 },
+  { &assh_sign_rsa_sha1_md5,      &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) - 1 },
+  { &assh_sign_rsa_sha1,          &assh_key_rsa, rsa1024_key, sizeof(rsa1024_key) - 1 },
+  { &assh_sign_rsa_sha1_2048,     &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) - 1 },
+  { &assh_sign_rsa_sha256,        &assh_key_rsa, rsa2048_key, sizeof(rsa2048_key) - 1 },
+  { &assh_sign_rsa_sha512,        &assh_key_rsa, rsa3072_key, sizeof(rsa3072_key) - 1 },
+  { &assh_sign_ed25519,           &assh_key_ed25519, ed25519_key, sizeof(ed25519_key) - 1 },
+  { NULL }
+};
 
-static const struct assh_algo_mac_s *mac_list[] =
-  {
-    &assh_hmac_none,
-    &assh_hmac_md5,
-    NULL
-  };
+static const struct algo_with_key_s sign_list_short[] =
+{
+  { &assh_sign_none,              &assh_key_none, (const uint8_t*)"\0", 0 },
+  { NULL }
+};
 
-static const struct assh_algo_compress_s *comp_list[] =
-  {
-    &assh_compress_none,
-    NULL
-  };
+static const struct assh_algo_mac_s *mac_list_long[] =
+{
+  &assh_hmac_none,
+# ifdef CONFIG_ASSH_HASH_MD5
+  &assh_hmac_md5,
+  &assh_hmac_md5_96,
+  &assh_hmac_md5_etm,
+  &assh_hmac_md5_96_etm,
+# endif
+# ifdef CONFIG_ASSH_HASH_SHA1
+  &assh_hmac_sha1,
+  &assh_hmac_sha1_96,
+  &assh_hmac_sha1_etm,
+  &assh_hmac_sha1_96_etm,
+# endif
+# ifdef CONFIG_ASSH_HASH_SHA2
+  &assh_hmac_sha256,
+  &assh_hmac_sha512,
+  &assh_hmac_sha256_etm,
+  &assh_hmac_sha512_etm,
+# endif
+# ifdef CONFIG_ASSH_HASH_RIPEMD160
+  &assh_hmac_ripemd160,
+# endif
+  NULL
+};
+
+static const struct assh_algo_mac_s *mac_list_short[] =
+{
+  &assh_hmac_md5,
+  NULL
+};
+
+static const struct assh_algo_cipher_s *cipher_list_long[] =
+{
+# ifdef CONFIG_ASSH_CIPHER_TDES
+  &assh_cipher_tdes_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_tdes_ctr,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_CAST128
+  &assh_cipher_cast128_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_cast128_ctr,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_IDEA
+  &assh_cipher_idea_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_idea_ctr,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_BLOWFISH
+  &assh_cipher_blowfish_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_blowfish_ctr,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_TWOFISH
+  &assh_cipher_twofish128_cbc,
+  &assh_cipher_twofish256_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_twofish128_ctr,
+  &assh_cipher_twofish256_ctr,
+#  endif
+#  ifdef CONFIG_ASSH_MODE_GCM
+  &assh_cipher_twofish128_gcm,
+  &assh_cipher_twofish256_gcm,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_SERPENT
+  &assh_cipher_serpent128_cbc,
+  &assh_cipher_serpent192_cbc,
+  &assh_cipher_serpent256_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_serpent128_ctr,
+  &assh_cipher_serpent192_ctr,
+  &assh_cipher_serpent256_ctr,
+#  endif
+#  ifdef CONFIG_ASSH_MODE_GCM
+  &assh_cipher_serpent128_gcm,
+  &assh_cipher_serpent256_gcm,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_ARCFOUR
+  &assh_cipher_arc4,
+  &assh_cipher_arc4_128,
+  &assh_cipher_arc4_256,
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_AES
+  &assh_cipher_aes128_cbc,
+  &assh_cipher_aes192_cbc,
+  &assh_cipher_aes256_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_aes128_ctr,
+  &assh_cipher_aes192_ctr,
+  &assh_cipher_aes256_ctr,
+#  endif
+#  ifdef CONFIG_ASSH_MODE_GCM
+  &assh_cipher_aes128_gcm,
+  &assh_cipher_aes256_gcm,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_CAMELLIA
+  &assh_cipher_camellia128_cbc,
+  &assh_cipher_camellia192_cbc,
+  &assh_cipher_camellia256_cbc,
+#  ifdef CONFIG_ASSH_MODE_CTR
+  &assh_cipher_camellia128_ctr,
+  &assh_cipher_camellia192_ctr,
+  &assh_cipher_camellia256_ctr,
+#  endif
+# endif
+
+# ifdef CONFIG_ASSH_CIPHER_CHACHAPOLY
+  &assh_cipher_chachapoly,
+# endif
+  NULL
+};
+
+static const struct assh_algo_cipher_s *cipher_list_short[] =
+{
+  &assh_cipher_none,
+  &assh_cipher_arc4,
+  &assh_cipher_aes256_ctr,
+  NULL
+};
+
+static const struct assh_algo_compress_s *comp_list_long[] =
+{
+  &assh_compress_none,
+# ifdef CONFIG_ASSH_USE_ZLIB
+  &assh_compress_zlib,
+  &assh_compress_zlib_openssh,
+# endif
+  NULL
+};
+
+static const struct assh_algo_compress_s *comp_list_short[] =
+{
+  &assh_compress_none,
+  NULL
+};
 
 #define TEST_FAIL(...)				\
   do {						\
@@ -137,7 +281,7 @@ int test(const struct assh_algo_kex_s *kex,
 
   uint_fast8_t i;
 
-  fprintf(stderr, "==== %s, %s, %s, %s, %s ====\n",
+  fprintf(stderr, "%36s, %30s, %30s, %30s, %30s\n",
 	  assh_algo_name(&kex->algo), assh_algo_name(&sign->algo),
 	  assh_algo_name(&cipher->algo), assh_algo_name(&mac->algo),
 	  assh_algo_name(&comp->algo));
@@ -208,6 +352,7 @@ int test(const struct assh_algo_kex_s *kex,
 	    }
 
 	    case ASSH_EVENT_CONNECTION_START:
+#warning transfer more data
 	      assh_session_cleanup(&session[0]);
 	      assh_context_cleanup(&context[0]);
 	      assh_session_cleanup(&session[1]);
@@ -229,36 +374,59 @@ int test(const struct assh_algo_kex_s *kex,
     }
 }
 
+int test_loop(const struct algo_with_key_s *kex,
+	      const struct algo_with_key_s *sign,
+	      const struct assh_algo_cipher_s **cipher,
+	      const struct assh_algo_mac_s **mac,
+	      const struct assh_algo_compress_s **comp)
+{
+  const struct algo_with_key_s *kex_list = kex;
+  const struct algo_with_key_s *sign_list = sign;
+  const struct assh_algo_cipher_s **cipher_list = cipher;
+  const struct assh_algo_mac_s **mac_list = mac;
+  const struct assh_algo_compress_s **comp_list = comp;
+
+  while (*comp)
+    {
+      while (*mac)
+	{
+	  while (*cipher)
+	    {
+	      while (sign->algo)
+		{
+		  while (kex->algo)
+		    {
+		      if (test(kex->algo, sign->algo, *cipher, *mac, *comp,
+			       kex, sign))
+			return -1;
+		      kex++;
+		    }
+		  kex = kex_list;
+		  sign++;
+		}
+	      sign = sign_list;
+	      cipher++;
+	    }
+	  cipher = cipher_list;
+	  mac++;
+	}
+      mac = mac_list;
+      comp++;
+    }
+
+  return 0;
+}
+
 int main()
 {
-  const struct algo_with_key_s *kex = kex_list;
-  const struct algo_with_key_s *sign = sign_list;
-  const struct assh_algo_cipher_s **cipher = cipher_list;
-  const struct assh_algo_mac_s **mac = mac_list;
-  const struct assh_algo_compress_s **comp = comp_list;
+  if (test_loop(kex_list_short, sign_list_short, cipher_list_long, mac_list_long, comp_list_short))
+    return -1;
 
-  while (*mac)
-    {
-      while (*cipher)
-	{
-	  while (sign->algo)
-	    {
-	      while (kex->algo)
-		{
-		  if (test(kex->algo, sign->algo, *cipher, *mac, *comp,
-			   kex, sign))
-		    return -1;
-		  kex++;
-		}
-	      kex = kex_list;
-	      sign++;
-	    }
-	  sign = sign_list;
-	  cipher++;
-	}
-      cipher = cipher_list;
-      mac++;
-    }
+  if (test_loop(kex_list_short, sign_list_short, cipher_list_short, mac_list_short, comp_list_long))
+    return -1;
+
+  if (test_loop(kex_list_long, sign_list_long, cipher_list_short, mac_list_short, comp_list_short))
+    return -1;
 
   return 0;
 }
