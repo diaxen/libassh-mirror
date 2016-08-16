e22d7ce6f531a0b9b02903d65d24b8e850a045dc
Alexandre Becoulet <alexandre.becoulet@free.fr>
2016-08-16 21:22:55 +0200

    test: display failed test location

--- test/test.h
+++ test/test.h
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TEST_LOC(lnum) __FILE__ ":" #lnum ":"
+#define TEST_LOC_(x) TEST_LOC(x)
+
 #define TEST_FAIL(...)				\
   do {						\
-    fprintf(stderr, "FAIL " __VA_ARGS__);	\
+    fprintf(stderr, "FAIL " TEST_LOC_(__LINE__) __VA_ARGS__);	\
     exit(1);					\
   } while (0)
 
