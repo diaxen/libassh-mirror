9d11df760e95f917ac924cbaa159c2c661b4b6f1
Alexandre Becoulet <alexandre.becoulet@free.fr>
2019-09-30 15:26:42 +0200

    test/signature2: use getopt, better fuzzing

--- test/signature2.c
+++ test/signature2.c
@@ -6,16 +6,24 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
+#include <getopt.h>
 
 #include "sign.h"
 #include "prng_weak.h"
 #include "test.h"
+#include "fuzz.h"
 
 #define TEST_STEP 4
 
 struct assh_context_s *context;
 
-assh_error_t test_sign(unsigned int max_size)
+enum action_e {
+  ACTION_NEW_KEYS = 1,
+  ACTION_VALIDATE_KEYS = 2,
+  ACTION_FUZZ_CHECK = 4,
+};
+
+assh_error_t test_sign(unsigned int max_size, enum action_e action)
 {
   assh_error_t err;
   int i;
@@ -38,9 +46,12 @@ assh_error_t test_sign(unsigned int max_size)
 				 key_blob[0], &kb, sizeof(key_blob) - 1));
 
 #ifdef CONFIG_ASSH_KEY_VALIDATE
-      enum assh_key_validate_result_e r;
-      ASSH_RET_ON_ERR(assh_key_validate(context, key2, &r));
-      TEST_ASSERT(r > 0);
+      if (action & ACTION_VALIDATE_KEYS)
+	{
+	  enum assh_key_validate_result_e r;
+	  ASSH_RET_ON_ERR(assh_key_validate(context, key2, &r));
+	  TEST_ASSERT(r > 0);
+	}
 #endif
 
       TEST_ASSERT(assh_key_cmp(context, key2, key2, 0));
@@ -52,7 +63,7 @@ assh_error_t test_sign(unsigned int max_size)
       for (size = max_size; size != 0; )
 	{
 #ifdef CONFIG_ASSH_KEY_CREATE
-	  if (algos[i].gen_key)
+	  if (algos[i].gen_key && (action & ACTION_NEW_KEYS))
 	    {
 	      size_t kbits = algos[i].kbits_min + assh_prng_rand()
                            % (algos[i].kbits_max - algos[i].kbits_min + 1);
@@ -62,6 +73,7 @@ assh_error_t test_sign(unsigned int max_size)
 
 # ifdef CONFIG_ASSH_KEY_VALIDATE
               fprintf(stderr, "C");
+	      enum assh_key_validate_result_e r;
 	      ASSH_RET_ON_ERR(assh_key_validate(context, key, &r));
 	      TEST_ASSERT(r > 0);
 # endif
@@ -111,27 +123,26 @@ assh_error_t test_sign(unsigned int max_size)
 
 	  TEST_ASSERT(sign_safety <= a->algo.safety && sign_safety <= key->safety);
 
-	  while (1)
+	  if (action & ACTION_FUZZ_CHECK)
 	    {
-	      unsigned int r1 = assh_prng_rand() % sign_len;
-	      unsigned char r2 = assh_prng_rand();
-	      r2 += !r2;
+	      unsigned mc, fc = 256;
 
-#ifdef CONFIG_ASSH_DEBUG_SIGN
-	      fprintf(stderr, "Mangling signature byte %u, previous=0x%02x, new=0x%02x\n",
-		      r1, sign[r1], sign[r1] ^ r2);
-#endif
-	      /* mangle */
-	      sign[r1] ^= r2;
+	      while (fc)
+		{
+		  uint8_t sign2[sign_len];
+		  memcpy(sign2, sign, sign_len);
 
-	      fprintf(stderr, "V");
+		  do {
+		    mc = aash_fuzz_mangle(sign2, sign_len, 10 + assh_prng_rand() % 1024);
+		  } while (!mc);
 
-	      err = assh_sign_check(context, a, key, c, d, sign, sign_len, &sign_safety);
-	      if (err != ASSH_OK)
-		break;   /* successfully broke the signature */
+		  fprintf(stderr, "V");
 
-	      /* restore */
-	      sign[r1] ^= r2;
+		  err = assh_sign_check(context, a, key, c, d, sign2, sign_len, &sign_safety);
+
+		  if (err != ASSH_OK)
+		    fc--;   /* successfully broke the signature */
+		}
 	    }
 
 	  if (size)
@@ -151,7 +162,7 @@ assh_error_t test_sign(unsigned int max_size)
 	    }
 
 #ifdef CONFIG_ASSH_KEY_CREATE
-	  if (algos[i].gen_key)
+	  if (algos[i].gen_key && (action & ACTION_NEW_KEYS))
 	    assh_key_drop(context, &key);
 #endif
 	}
@@ -162,27 +173,90 @@ assh_error_t test_sign(unsigned int max_size)
   return ASSH_OK;
 }
 
+static void usage()
+{
+  fprintf(stderr, "usage: signature2 [options]\n");
+
+  fprintf(stderr,
+	  "Options:\n\n"
+
+	  "    -h         show help\n"
+#ifdef CONFIG_ASSH_KEY_CREATE
+	  "    -n         test key creation\n"
+#endif
+#ifdef CONFIG_ASSH_KEY_VALIDATE
+	  "    -v         test key validation\n"
+#endif
+	  "    -f         fuzz signature checking\n"
+	  "    -m size    set the payload max size (default 128)\n"
+	  "    -c count   set number of test passes (default 1)\n"
+	  "    -s seed    set initial seed (default: time(0))\n"
+	  );
+}
+
 int main(int argc, char **argv)
 {
-  unsigned int s = argc > 1 ? atoi(argv[1]) : time(0);
   assh_error_t err;
-  int i;
 
   if (assh_deps_init())
     return -1;
+
+  enum action_e action = 0;
+  unsigned seed = time(0);
+  unsigned count = 1;
+  unsigned max_size = 128;
+  int opt;
+
+  while ((opt = getopt(argc, argv, "nvfhs:c:m:")) != -1)
+    {
+      switch (opt)
+	{
+#ifdef CONFIG_ASSH_KEY_CREATE
+	case 'n':
+	  action |= ACTION_NEW_KEYS;
+	  break;
+#endif
+#ifdef CONFIG_ASSH_KEY_VALIDATE
+	case 'v':
+	  action |= ACTION_VALIDATE_KEYS;
+	  break;
+#endif
+	case 'f':
+	  action |= ACTION_FUZZ_CHECK;
+	  break;
+	case 's':
+	  seed = atoi(optarg);
+	  break;
+	case 'c':
+	  count = atoi(optarg);
+	  break;
+	case 'm':
+	  max_size = atoi(optarg);
+	  break;
+	case 'h':
+	  usage();
+	default:
+	  return 1;
+	}
+    }
+
+  if (!action)
+    action = ACTION_NEW_KEYS | ACTION_VALIDATE_KEYS;
 
   if (assh_context_create(&context, ASSH_CLIENT_SERVER,
 			NULL, NULL, &assh_prng_dummy, NULL))
     return -1;
 
+  unsigned i;
   for (i = 0; algos[i].algo; i++)
     ASSH_RET_ON_ERR(assh_algo_register_va(context, 0, 0, 0, algos[i].algo, NULL));
 
-  assh_prng_seed(s);
-  fprintf(stderr, "Seed: %u", s);
+  assh_prng_seed(seed);
+  fprintf(stderr, "Seed: %u", seed);
 
-  if (test_sign(128))
-    return 2;
+  while (count--)
+    if (test_sign(max_size, action))
+      return 2;
 
   assh_context_cleanup(context);
 
